14c117aaa0818ffba5c317d6924a53ff07b644fd
megafemworld <144464678+megafemworld@users.noreply.github.com>
2025-01-19 07:14:53 +0100

    unit controller almost done

--- app/Http/Controllers/UnitController.php
+++ app/Http/Controllers/UnitController.php
@@ -15,4 +15,53 @@ class UnitController extends BaseController
         ];
         return view('portal.units', $data);
     }
+
+    public function store()
+    {
+        $units = Unit::all();
+
+        return view('portal.createUnits', [
+            'units' => $units,
+        ]);
+
+        return view('portal.createUnits', $data);
+    }
+
+    public function createUnit(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+        ]);
+
+        $unit = Unit::where('name', $validatedData['title'])->first();
+
+        if ($unit) {
+            // Error Unit already exists
+
+        }
+        else {
+            $unitData = [
+                'name' => $validatedData['title'],
+            ];
+
+            $unit = Unit::create($unitData);
+        }
+
+    }
+
+    public function edit(Unit $unit)
+    {
+        return view('units.edit', compact('unit'));
+    }
+
+    public function update(Request $request, Unit $unit)
+    {
+        $validatedData = $request->validate( [
+            'title' => 'required|string|max:255',
+        ]);
+
+        $unit->update($validatedData);
+
+        return redirect()->route('units')->('success', 'Unit updated successfully!');
+    }
 }
