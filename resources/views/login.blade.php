1a5cdb365c4884c671ddbcac91c4b575ae8baa5c
Femi Mehalayese <megafemclick@gmail.com>
2025-03-02 18:07:13 +0100

    login and order dashboard

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -25,14 +25,6 @@
                                 dashboard
                             </p>
 
-                            @if (session('error'))
-                                <div class="alert alert-danger">
-                                    <ul>
-                                        <li>{{ session('error') }}</li>
-                                    </ul>
-                                </div>
-                            @endif
-
                             @if ($errors->any())
                                 <div class="alert alert-danger">
                                     <ul>
