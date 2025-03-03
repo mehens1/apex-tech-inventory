f1700ae9ea12230eaa4f7e88ae8bfa7c2beab98e
Femi Mehalayese <megafemclick@gmail.com>
2025-03-03 16:29:03 +0100

    user register and reset done with success message

--- resources/views/password-reset.blade.php
+++ resources/views/password-reset.blade.php
@@ -24,6 +24,12 @@
                             <p class="account-subtitle mt-3">APEX Solar & CCTV Inventory System <br> Rest password
                             </p>
 
+                            @if(session('status'))
+                                <div class="alert alert-success">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+
                             @if ($errors->any())
                                 <div class="alert alert-danger">
                                     <ul>
